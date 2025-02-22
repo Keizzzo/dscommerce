3e6c5dc074674e60677c6a19029caf21ccdf428e
Keizo Kobayashi <keizo.kobayashi@outlook.com.br>
2025-02-22 20:17:12 -0300

    Get Products Using Pageable Feature

--- src/main/java/com/devsuperior/_dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/_dscommerce/controllers/ProductController.java
@@ -5,11 +5,14 @@ import com.devsuperior._dscommerce.entities.Product;
 import com.devsuperior._dscommerce.repositories.ProductRepository;
 import com.devsuperior._dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -24,6 +27,11 @@ public class ProductController {
         ProductDTO result = service.findById(id);
 
         return result;
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
     }
 
 }
--- src/main/java/com/devsuperior/_dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/_dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.devsuperior._dscommerce.dto.ProductDTO;
 import com.devsuperior._dscommerce.entities.Product;
 import com.devsuperior._dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,6 +22,15 @@ public class ProductService {
     public ProductDTO findById(Long id){
         Optional<Product> result = repository.findById(id);
         return new ProductDTO(result.get());
+
+    }
+
+    // Pageable and Page import should be from springframework data domain
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        // Page is already a stream() from Java
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(product -> new ProductDTO(product));
 
     }
 
